95a28f4e734939332c518e2c80e1e1bca15d1372
Afranio Caires <137647669+AfranioCaires@users.noreply.github.com>
2024-02-21 15:45:21 -0300

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/BestBrowserGames/",
+  base: "/BestBrowserGames",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
